55431ff7ce5d5ab5f8485744924b9c8d7413babe
Tanner <tdyer@hawk.iit.edu>
2021-01-05 18:40:09 -0600

    created options menu for timer creation

--- app/src/main/java/com/example/simultimer/MainActivity.java
+++ app/src/main/java/com/example/simultimer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.simultimer;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         timerThread = new Thread(timer);
     }
 
+
+
+    // timer management functions
+
+    public void createTimer() {
+
+    }
+
     public void startTimer(View v) {
         timerThread.start();
     }
@@ -28,5 +38,25 @@ public class MainActivity extends AppCompatActivity {
         double rem = timer.getRemaining();
         timer = new TimerRunnable(rem, timer.getName());
         timerThread = new Thread(timer);
+    }
+
+
+
+    // create timer menu functions
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.menuCreateTimer) {
+            createTimer();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
