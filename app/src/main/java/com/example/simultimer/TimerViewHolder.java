f5ab07e8900e67c0d6cd841c197c188bf00badb3
Tanner <tdyer@hawk.iit.edu>
2021-01-12 21:10:34 -0600

    implemented timer pausing functionality

--- app/src/main/java/com/example/simultimer/TimerViewHolder.java
+++ app/src/main/java/com/example/simultimer/TimerViewHolder.java
@@ -3,7 +3,8 @@ package com.example.simultimer;
 /*
 
     ViewHolder class that stores contains layout elements for each
-    timer in the RecyclerView in the MainActivity
+    timer in the RecyclerView in the MainActivity. Also handles
+    timer starting/pausing when the corresponding button is selected.
 
 */
 
@@ -21,24 +22,47 @@ public class TimerViewHolder extends RecyclerView.ViewHolder implements View.OnC
 
     TimerViewHolder(@NonNull View view) {
         super(view);
+
+        // create references to layout objects from timer_recycler_element.xml layout
         title = view.findViewById(R.id.timerTitle);
         timeRemaining = view.findViewById(R.id.timeRemaining);
         startButton = view.findViewById(R.id.startTimer);
         pauseButton = view.findViewById(R.id.pauseTimer);
+
+        // set on-click listeners for start/pause buttons
         startButton.setOnClickListener(this);
         pauseButton.setOnClickListener(this);
     }
 
+
+    // on-click method that is called whenever a timers start/pause button is pressed
     @Override
     public void onClick(View view) {
 
+        // obtain references to the selected timer and its execution thread
         TimerRunnable selectedTimer = MainActivity.timerRunnables.get(getAdapterPosition());
         Thread selectedTimerThread = MainActivity.timerThreads.get(selectedTimer);
 
-        if (view.getId() == startButton.getId()) {
-            Log.d("VIEWHOLDER", "onClick: starting " + selectedTimerThread.getName());
-        } else if (view.getId() == pauseButton.getId()) {
-            Log.d("VIEWHOLDER", "onClick: pausing " + selectedTimerThread.getName());
+        if (selectedTimerThread != null) {
+            if (view.getId() == startButton.getId()) { // start button pressed
+                // start the selected timer's thread
+                selectedTimerThread.start();
+            } else if (view.getId() == pauseButton.getId()) { // pause button pressed
+
+                // interrupt the timer's thread execution and make note of remaining time
+                selectedTimerThread.interrupt();
+                double rem = selectedTimer.getRemaining();
+
+                // create new runnable + thread objects starting using the remaining time on the old thread
+                TimerRunnable newTimer = new TimerRunnable(rem, selectedTimer.getName());
+                Thread newThread = new Thread(newTimer);
+
+                // update the timer storage data structures on MainActivity with new objects
+                MainActivity.timerRunnables.set(getAdapterPosition(), newTimer);
+                MainActivity.timerThreads.remove(selectedTimer);
+                MainActivity.timerThreads.put(newTimer, newThread);
+
+            }
         }
     }
 }
